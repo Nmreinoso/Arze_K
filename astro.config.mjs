c9e2ec007fbf8a4778c188ece9159b73164c2e88
Nmreinoso <nmreinoso@wm.edu>
2026-03-06 13:15:25 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://Nmreinoso.github.io',
   base: '/Arze_K',
-  trailingSlash: 'always', // This helps GitHub Pages find the right folders
 });
